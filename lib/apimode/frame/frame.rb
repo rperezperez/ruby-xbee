b1ee55e56128d44ec4356850c15f14d1b3581087
Exile <exile@chamber.ee>
2013-03-21 18:11:51 +0200

    Fix escaping
    This function didn't work in ruby 1.9

--- lib/apimode/frame/frame.rb
+++ lib/apimode/frame/frame.rb
@@ -2,7 +2,7 @@ $: << File.dirname(__FILE__)
 
 class String
   def xb_escape
-    self.gsub(/[\176\175\021\023]/) { |c| [0x7D, c[0] ^ 0x20].pack("CC")}
+    self.gsub(/[\176\175\021\023]/) { |c| [0x7D, c[0].ord ^ 0x20].pack("CC")}
   end
   def xb_unescape
     self.gsub(/\175./) { |ec| [ec.unpack("CC").last ^ 0x20].pack("C")}
